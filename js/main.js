84f07df800315c9a6facd8ef7606026dbde353b5
DezzaAnnePeregrina <peregrinadezzaanne@gmail.com>
2024-02-01 17:12:18 +0800

    modified testimonials

--- js/main.js
+++ js/main.js
@@ -56,13 +56,6 @@
     });
 
 
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
-
-
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
         autoplay: false,
